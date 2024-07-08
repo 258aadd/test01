cd48a97277fb0b2b31838f87dc91859879668570
258aadd <148954448+258aadd@users.noreply.github.com>
2024-07-08 16:31:33 +0800

    Delete hospitalManager directory

--- hospitalManager/src/main/java/com/ml/pojo/Consultation.java
+++
@@ -1,5 +0,0 @@
-package com.ml.pojo;
-
-public class Consultation {
-
-}
--- hospitalManager/src/main/java/com/ml/service/AdminsService.java
+++
@@ -1,9 +0,0 @@
-package com.ml.service;
-
-import com.ml.pojo.Admins;
-
-import java.sql.SQLException;
-
-public interface AdminsService {
-    Admins login(String name, String password);
-}
